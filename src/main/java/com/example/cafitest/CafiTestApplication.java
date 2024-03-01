3cf07325e3f221e9e106ae70a068101e2dd397d9
Cheke <chekelosos@gmail.com>
2024-03-01 06:29:00 -0600

    Initial commit

---
+++ src/main/java/com/example/cafitest/CafiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafiTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cafitest/CafiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
